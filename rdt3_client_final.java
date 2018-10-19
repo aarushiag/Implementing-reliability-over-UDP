60ec4cb39ed4c5b03fae719db4460379bff86049
Aarushi Agarwal <aarushi16216@iiitd.ac.in>
2018-10-19 19:56:50 +0530

    Add files via upload
    UDP along with seq numbers and acknowledgements

--- rdt3_client_final.java
+++ rdt3_client_final.java
@@ -18,15 +18,15 @@ class object implements Serializable
 {
 	String message;
 	int seq_num;
-	int window_size;
+	 
 	int my_port;
 	String my_ip;
 	int ack;
-	public object(String message,int seq_num,int window_size,int my_port,String my_ip,int ack)
+	public object(String message,int seq_num, int my_port,String my_ip,int ack)
 	{
 		this.message=message;
 		this.seq_num=seq_num;
-		this.window_size = window_size;
+		 
 		this.my_port = my_port;
 		this.my_ip = my_ip;
 		this.ack = ack;
@@ -34,95 +34,152 @@ class object implements Serializable
 
 }
 
+//class MyThread extends Thread {
+//
+//    public void run(){
+//    	while(true)
+//       {System.out.println("MyThread running");}
+//    }
+//  }
+
 public class rdt3_client_final
 { 
-public static void main(String args[]) throws IOException 
-{ 
-   Scanner sc = new Scanner(System.in); 
-   
-     System.out.println("Enter the port number you want to connect at :- ");
-	 int port = sc.nextInt();
-	 sc.nextLine();
-	 System.out.println("Enter the ip address you want to connect at :- ");
-	 String ip_addr = sc.nextLine();
-	 
-	 System.out.println("Enter the window size :- ");
-	 int n = sc.nextInt();
-	 
-	 System.out.println("Enter the port number you want to listen at :- ");
-	 int listen_port = sc.nextInt();
-	 sc.nextLine();
-	 
-	 System.out.println("Enter the your ip address :- ");
-	 String my_ip = sc.nextLine();
+	
+	 static int packet_num = 0;
 
-   DatagramSocket ds = new DatagramSocket(listen_port); 
-   InetAddress ip = InetAddress.getByName(ip_addr); 
+	 
+	 
+	public static void main(String args[]) throws IOException 
+	{ 
+	   Scanner sc = new Scanner(System.in); 
+	    
+	   
+	     System.out.println("Enter the port number you want to connect at :- ");
+		 int port = sc.nextInt();
+		 sc.nextLine();
+		 System.out.println("Enter the ip address you want to connect at :- ");
+		 String ip_addr = sc.nextLine();
+		 
+		 System.out.println("Enter the window size :- ");
+		 int n = sc.nextInt();
+		 
+		 System.out.println("Enter the port number you want to listen at :- ");
+		 int listen_port = sc.nextInt();
+		 sc.nextLine();
+		 
+		 System.out.println("Enter the your ip address :- ");
+		 String my_ip = sc.nextLine();
+	
+	   DatagramSocket receiving_port = new DatagramSocket(listen_port);
+	   DatagramSocket sending_port = new DatagramSocket(8000);
+	   
+	   InetAddress ip = InetAddress.getByName(ip_addr); 
+	   
+	
+	   //To read the acknowledgements
+//	   MyThread myThread = new MyThread();
+//	   myThread.start();
+	
+	   Thread t1 = new Thread(){
+	         public void run(){
+	      	 for (int i=0;i<20000;i++)
+	  		 {
+	      		 
+	  			 String inp = Integer.toString(i);
+	  			 object packet = new object(inp,packet_num,listen_port,my_ip,0);		 
+	  			 ByteArrayOutputStream bos = new ByteArrayOutputStream();
+	  		     ObjectOutputStream oos;
+				try {
+					oos = new ObjectOutputStream(bos);
+					oos.writeObject(packet);
+					
+					 byte buf[] = bos.toByteArray();
+		  			 
+			  	     DatagramPacket DpSend = 
+			  	             new DatagramPacket(buf, buf.length, ip, port); 
+			  	       sending_port.send(DpSend); 
+			  	
+			  	     packet_num = (packet_num+1)%n;
+			  	       
+				} catch (IOException e2) {
+					// TODO Auto-generated catch block
+					e2.printStackTrace();
+				}	  		      
+	  			
    
-   int packet_num = 0;
+	  		 }
+	      	 System.out.println("helloo");
+	         }
+	     };
+	     
+	     
+	     Thread t2 = new Thread(){
+	    	 
+	         public void run(){	        	 
+	        	 
+		  	       
+		  	       //Read acknowledgement
+	        	 while(true)
+	        	 {
+		  	       
+		  	        byte[] receive = new byte[2048];
+		  	        DatagramPacket  DpReceive = new DatagramPacket(receive, receive.length); 
+		  			    	 
+		  	        try 
+		  	        {
+		  			    receiving_port.receive(DpReceive);
+		  			    ByteArrayInputStream in = new ByteArrayInputStream(receive);
+		  			    ObjectInputStream is = new ObjectInputStream(in);
+		  			  
+		  			    object ack_packet = (object)is.readObject();   
+		  			     
+		  			    		 
+		  			    		
+		  			    System.out.println("Ack from server for:-"+ack_packet.seq_num+" "+ ack_packet.ack);
+		  			     
+		  			    		 
+		  		 	 }
+		  	       
+		  	       	catch (IOException e2) {
+						// TODO Auto-generated catch block
+						e2.printStackTrace();
+					}
+		  			  	 	 
+		  		 	catch (ClassNotFoundException e) 
+		  		 	{
+		  				 e.printStackTrace();
+		  			}
+		  	       
+		  	       
+	        	 }	 
+	             
+	         }
+	     };
+	 
+	     t2.start();
+	     t1.start();
+	     
+	
 
-  // loop while user not enters "bye" 
-   for (int i=0;i<20000;i++)
-	 {
-		 String inp = Integer.toString(i);
-		 packet_num = (packet_num+1)%n;
-		 object packet = new object(inp,packet_num,n,listen_port,my_ip,0);		 
-		 ByteArrayOutputStream bos = new ByteArrayOutputStream();
-	     ObjectOutputStream oos = new ObjectOutputStream(bos);
-	     oos.writeObject(packet);
-	     oos.flush();
-		 byte buf[] = bos.toByteArray();
-		 
-       DatagramPacket DpSend = 
-             new DatagramPacket(buf, buf.length, ip, port); 
-       ds.send(DpSend); 
-       
-       //Read acknowledgement
-       
-       byte[] receive = new byte[2048];
-       DatagramPacket  DpReceive = new DatagramPacket(receive, receive.length); 
-		    	 
-       try 
-       {
-		    ds.receive(DpReceive);
-		    ByteArrayInputStream in = new ByteArrayInputStream(receive);
-		    ObjectInputStream is = new ObjectInputStream(in);
-		  
-		    object ack_packet = (object)is.readObject();   
-		     
-		    		 
-		    		
-		    System.out.println("Ack from server for:-"+i+ ack_packet.ack);
-		                 
-		    receive = new byte[2048]; 
-		    		 
-	 	 }
-		  	 	 
-	 	 catch (ClassNotFoundException e) 
-	 	 {
-		 		 e.printStackTrace();
-		 }
-       
-	 }
-   while (true) 
-   { 
-       String inp = sc.nextLine(); 
-       packet_num = (packet_num+1)%n;
-       
-       object packet = new object(inp,packet_num,n,listen_port,my_ip,0);		 
-       ByteArrayOutputStream bos = new ByteArrayOutputStream();
-	   ObjectOutputStream oos = new ObjectOutputStream(bos);
-	   oos.writeObject(packet);
-	   oos.flush();
-	   byte buf[] = bos.toByteArray();       
-         
-		 
-       DatagramPacket DpSend = 
-             new DatagramPacket(buf, buf.length, ip, port); 
-       ds.send(DpSend);
-       
-       if (inp.equals("bye")) 
-         break; 
-   } 
-} 
+	   while (true) 
+	   { 
+	//       String inp = sc.nextLine(); 
+	//       packet_num = (packet_num+1)%n;
+	//       
+	//       object packet = new object(inp,packet_num,n,listen_port,my_ip,0);		 
+	//       ByteArrayOutputStream bos = new ByteArrayOutputStream();
+	//	   ObjectOutputStream oos = new ObjectOutputStream(bos);
+	//	   oos.writeObject(packet);
+	//	   oos.flush();
+	//	   byte buf[] = bos.toByteArray();       
+	//         
+	//		 
+	//       DatagramPacket DpSend = 
+	//             new DatagramPacket(buf, buf.length, ip, port); 
+	//       receiving_port.send(DpSend);
+	//       
+	//       if (inp.equals("bye")) 
+	//         break; 
+	   } 
+	} 
 } 
--- rdt3_server_final.java
+++ rdt3_server_final.java
@@ -23,15 +23,15 @@ class object implements Serializable
 {
 	String message;
 	int seq_num;
-	int window_size;
+	 
 	int my_port;
 	String my_ip;
 	int ack;
-	public object(String message,int seq_num,int window_size,int my_port,String my_ip,int ack)
+	public object(String message,int seq_num,int my_port,String my_ip,int ack)
 	{
 		this.message=message;
 		this.seq_num=seq_num;
-		this.window_size = window_size;
+		 
 		this.my_port = my_port;
 		this.my_ip = my_ip;
 		this.ack = ack;
@@ -41,124 +41,123 @@ class object implements Serializable
 
 public class rdt3_server_final
 { 
+ 
+	static volatile int pointer =-1;
 
-static int pointer =0;
-static int window_size =0;
-static int client_port =0;
-static  InetAddress  client_address;
-
-public static void main(String[] args) throws IOException 
-{ 
-   // Step 1 : Create a socket to listen at specified port 
-	 System.out.println("Enter the port number you want to listen at :- ");
-	 Scanner obj = new Scanner(System.in);
-	 int port = obj.nextInt();
-   DatagramSocket ds = new DatagramSocket(port); 
-   byte[] receive = new byte[2048]; 
-   
-   DatagramPacket DpReceive = null; 
-   
-   for(int i=0;i<20000;i++)
-   {
-  	 
-  	 DpReceive = new DatagramPacket(receive, receive.length); 
-
-       
-  	 	try {
-	    		 ds.receive(DpReceive);
-	    		 ByteArrayInputStream in = new ByteArrayInputStream(receive);
-	    		 ObjectInputStream is = new ObjectInputStream(in);
-	    		 
-	    		object packet = (object)is.readObject();   
-	    		String data = packet.message;
-	    		window_size = packet.window_size;
-	    		client_port = packet.my_port;
-	    		client_address = InetAddress.getByName(packet.my_ip);
-	    		if(Integer.parseInt(data)==pointer)
-	    		{
-	    		   	 System.out.println("Client:-" + data);
-	    		   	 pointer++;
-	    		   	 
-	    		   	 //Send Acknowledgement
-	    		   	 object ack_packet = new object(null,0,0,0,null,1);		 
+	
+	public static void main(String[] args) throws IOException 
+	{ 
+		int window_size =0;
+		int client_port =0;
+		InetAddress  client_address;
+		 
+		
+		
+		System.out.println("Enter the port number you want to listen at :- ");
+		Scanner obj = new Scanner(System.in);
+		int port = obj.nextInt();
+		 
+		System.out.println("Enter the window size :- ");
+		int n = obj.nextInt();
+		 
+		 
+	    DatagramSocket receiving_port = new DatagramSocket(port);
+	    DatagramSocket sending_port = new DatagramSocket(8080);
+	    
+	    byte[] receive = new byte[50000]; 
+	   
+	    DatagramPacket DpReceive = null; 
+	   
+	//   for(int i=0;i<20000;i++)
+	    while(true)
+	    {
+	  	 
+	  	 DpReceive = new DatagramPacket(receive, receive.length); 
+	
+	       
+	  	 	try {
+		    		receiving_port.receive(DpReceive);
+		    		ByteArrayInputStream in = new ByteArrayInputStream(receive);
+		    		ObjectInputStream is = new ObjectInputStream(in);
+		    		 
+		    		object packet = (object)is.readObject();   
+		    		String data = packet.message;
+		    		client_port = packet.my_port;
+		    		client_address = InetAddress.getByName(packet.my_ip);
+		    		System.out.println(data);
+		    		
+		    		if(packet.seq_num == (pointer+1)%n)
+		    		{
+		    		   	 System.out.println("Client:-" + data);		    		    
+		    		     pointer=(pointer+1)%n;
+		    		}
+		    		
+		    		 //Send Acknowledgement 
+	    		   	 object ack_packet = new object(null,packet.seq_num,0,null,pointer);		 
 	    			 ByteArrayOutputStream bos = new ByteArrayOutputStream();
 	    		     ObjectOutputStream oos = new ObjectOutputStream(bos);
-	    		     oos.writeObject(packet);
+	    		     oos.writeObject(ack_packet);
 	    		     oos.flush();
 	    			 byte buf[] = bos.toByteArray();
 	    			 
 	    			 DatagramPacket DpSend = 
 	    		             new DatagramPacket(buf, buf.length, client_address, client_port); 
-	    		       ds.send(DpSend);
-	    		}
-	    		else
-	    		{
-	    		            	 
-	    		   	 for(int j=0;j<Integer.parseInt(data)-pointer;j++)
-	    		   	 {System.out.println("Packet lost! :(((");}
-	    		   	 System.out.println("Client:-" + data); 
-	    		   	 pointer=Integer.parseInt(data)+1;
-	    		}  
-	    		             
-	    		receive = new byte[2048]; 
-	    		ds.setSoTimeout(10000);
-	    		
-	    		 
-  		 }
-  	 	  catch(SocketTimeoutException e)
-	  		  {
-	  		   		for(int j=pointer;j<20000;j++)
-	  		  		{
-	  		 		  System.out.println("Packet lost! :(((");
-	  		    	}
-	  		    	break;
-	  		   }
-  	 	 catch (ClassNotFoundException e) 
-  	 	 {
-  	 		 e.printStackTrace();
-  		 }
-//  		 String data = new String(receive);
-//  		 data = data.trim();   		 
-  		 
-  	      
-  	 
-   }
-   
-   ds.setSoTimeout(0);
-   while (true) 
-   { 
-
-  	 DpReceive = new DatagramPacket(receive, receive.length); 
-  	 
-  	 try 
-  	 {
-  		ds.receive(DpReceive);
-  		ByteArrayInputStream in = new ByteArrayInputStream(receive);
-  		ObjectInputStream is = new ObjectInputStream(in);
-  		 
-  		object packet = (object)is.readObject();   
-  		String data = packet.message;
-  		window_size = packet.window_size;
-		client_port = packet.my_port;
-		client_address = InetAddress.getByName(packet.my_ip);
-  		if (data.equals("bye")) 
-  	    { 
-  	        System.out.println("Client EXITING"); 
-  	        break; 
-  	    } 
-  		
-  		System.out.println("Client:-" + data);
-  		             
-  		receive = new byte[2048]; 
-  		 
-		 }
-	 	 
-	 	 catch (ClassNotFoundException e) 
-	 	 {
-	 		 e.printStackTrace();
-		 }
-  	 
-   } 
-} 
+	    		       sending_port.send(DpSend);
+		    		             
+		    		receive = new byte[50000]; 
+		    		
+		    		
+		    	 
+		    		
+		    		 
+	  		 }
+	  	 	  
+	  	 	 catch (ClassNotFoundException e) 
+	  	 	 {
+	  	 		 e.printStackTrace();
+	  		 }
+	//  		 String data = new String(receive);
+	//  		 data = data.trim();   		 
+	  		 
+	  	      
+	  	 
+	    }
+	   
+	 //  receiving_port.setSoTimeout(0);
+	 //  while (true) 
+	//   { 
+	
+	//  	 DpReceive = new DatagramPacket(receive, receive.length); 
+	//  	 
+	//  	 try 
+	//  	 {
+	//  		receiving_port.receive(DpReceive);
+	//  		ByteArrayInputStream in = new ByteArrayInputStream(receive);
+	//  		ObjectInputStream is = new ObjectInputStream(in);
+	//  		 
+	//  		object packet = (object)is.readObject();   
+	//  		String data = packet.message;
+	//  		window_size = packet.window_size;
+	//		client_port = packet.my_port;
+	//		client_address = InetAddress.getByName(packet.my_ip);
+	//  		if (data.equals("bye")) 
+	//  	    { 
+	//  	        System.out.println("Client EXITING"); 
+	//  	        break; 
+	//  	    } 
+	//  		
+	//  		System.out.println("Client:-" + data);
+	//  		             
+	//  		receive = new byte[2048]; 
+	//  		 
+	//		 }
+	//	 	 
+	//	 	 catch (ClassNotFoundException e) 
+	//	 	 {
+	//	 		 e.printStackTrace();
+	//		 }
+	//  	 
+//	     } 
+	} 
 
 } 
